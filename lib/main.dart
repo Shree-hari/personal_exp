2d4d1ecc16ae38a97b995da761700aca955040ac
Harsh <harshmacc@gmail.com>
2020-03-04 06:02:27 +0530

    learn how to use LayoutBuilder widget first we need to pass a function with context and constraints as parameter and later on giving the dynamic height by using constraints.maxhight * (the_amount_of_ratio_to_be_covered_by_that _widget_inside_its_parent_widget) so this will set the height of the elements dynamically inside the widget.

--- lib/main.dart
+++ lib/main.dart
@@ -122,7 +122,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.4,
+                  0.3,
               width: double.infinity,
               child: Chart(_recentTransactions),
             ),
@@ -130,7 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
               height: (MediaQuery.of(context).size.height -
                       appBar.preferredSize.height -
                       MediaQuery.of(context).padding.top) *
-                  0.6,
+                  0.7,
               child: TransactionList(_userTransactions, _deleteTransaction),
             ),
           ],
