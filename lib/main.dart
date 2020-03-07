999b73a3e3bd89f1b39df16f90a951aea35c2b99
Harsh <harshmacc@gmail.com>
2020-03-07 15:47:00 +0530

    refactored the code

--- lib/main.dart
+++ lib/main.dart
@@ -100,7 +100,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final isLandscapeMode = MediaQuery.of(context).orientation == Orientation.landscape;
+    final mediaQuery = MediaQuery.of(context);
+    final isLandscapeMode = mediaQuery.orientation == Orientation.landscape;
     final appBar = AppBar(
       title: Text('Personal Expenses'),
       actions: <Widget>[
@@ -112,9 +113,9 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
     );
     final txListWidget = Container(
-      height: (MediaQuery.of(context).size.height -
+      height: (mediaQuery.size.height -
           appBar.preferredSize.height -
-          MediaQuery.of(context).padding.top) *
+          mediaQuery.padding.top) *
           0.7,
       child: TransactionList(_userTransactions, _deleteTransaction),
     );
@@ -147,9 +148,9 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             if(!isLandscapeMode)
               Container(
-                height: (MediaQuery.of(context).size.height -
+                height: (mediaQuery.size.height -
                     appBar.preferredSize.height -
-                    MediaQuery.of(context).padding.top) *
+                    mediaQuery.padding.top) *
                     0.3,
                 child: Chart(_recentTransactions),
               ),
@@ -157,9 +158,9 @@ class _MyHomePageState extends State<MyHomePage> {
               txListWidget,
             if(isLandscapeMode)
             _showChart ? Container(
-              height: (MediaQuery.of(context).size.height -
+              height: (mediaQuery.size.height -
                       appBar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
+                      mediaQuery.padding.top) *
                   0.7,
               child: Chart(_recentTransactions),
             ): txListWidget
